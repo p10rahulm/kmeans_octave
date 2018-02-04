bed338180e5928dcdbd8b14dd627a24b57834b22
Rahul Madhavan <rahul.maddy@gmail.com>
2018-02-04 22:47:09 +0530

    changed output of file to float

--- output_array_to_file.m
+++ output_array_to_file.m
@@ -1,7 +1,7 @@
 function output_array_to_file(arr,outputfile_path)
   mfile = fopen(outputfile_path, 'w+');
   for i=1:size(arr, 1)
-      fprintf(mfile, '%d ', arr(i,:));
+      fprintf(mfile, '%f ', arr(i,:));
       fprintf(mfile, '\n');
   end
   fclose(mfile);
